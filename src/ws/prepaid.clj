e253c254099c5e26ec6ab54de28f67934b86ba99
Diego Sanchez <disanchez@fi.uba.ar>
2021-01-27 10:34:17 -0300

    [project] Initialization & setup for the clojure project

---
+++ src/ws/prepaid.clj
@@ -0,0 +1,6 @@
+(ns ws.prepaid)
+
+(defn card
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/ws/prepaid_test.clj
@@ -0,0 +1,7 @@
+(ns ws.prepaid-test
+  (:require [clojure.test :refer :all]
+            [ws.prepaid :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
